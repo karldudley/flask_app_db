66c9441001f43fdcbead928a51f69e379933fe21
karl dudley <karldudley@gmail.com>
2022-10-24 16:16:47 +0100

    add table name

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ with app.app_context():
 # db.init_app(app)
 # Create db model
 class Friends(db.Model):
-    # id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'friends'
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
